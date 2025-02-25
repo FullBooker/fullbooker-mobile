7f73501327f56f6ea6c490d5d606df0ba25bce9c
Abiud Orina <abiudrn@gmail.com>
2025-02-25 21:13:23 +0300

    feat: cleanup test folder
    - set coverage threshold to 0.7%
    - remove unused tests
    - add coverage checker file
    - add test for get initials function
    - setup test folders properly

---
+++ check_coverage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import re
+import string
+import sys
+
+from bs4 import BeautifulSoup
+
+THRESHOLD = 0.7
+
+current_dir = os.getcwd()
+coverage_file_path = os.path.join(current_dir, "coverage/index.html")
+cov = None
+
+with open(coverage_file_path) as file:
+    soup = BeautifulSoup(file, "lxml")
+    covHi = soup.find(attrs={"class": "headerCovTableEntryHi"})
+    covMed = soup.find(attrs={"class": "headerCovTableEntryMed"})
+    covLow = soup.find(attrs={"class": "headerCovTableEntryLo"})
+
+    if hasattr(covHi, "string") and covHi.string != "-":
+        cov = covHi.string
+    elif hasattr(covMed, "string") and covMed.string != "-":
+        cov = covMed.string
+    elif hasattr(covLow, "string") and covLow.string != "-":
+        cov = covLow.string
+
+pattern = r"[{}]".format(string.punctuation)
+cov = re.sub(pattern, "", cov).strip().lower()
+cov = round(float(cov), 1)
+cov = (cov / 1000) * 100
+
+if cov < THRESHOLD:
+    print(f"Test coverage threshold {cov} is lower than expected {THRESHOLD}%")
+    sys.exit(1)
+else:
+    print(f"Test coverage is ok {cov}%")
+    sys.exit(0)
---
+++ test/unit_tests/core/utils_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fullbooker/core/utils.dart';
+
+void main() {
+  group('getInitials', () {
+    test('handles initials in the input', () {
+      expect(getInitials(''), ''); // empty
+      expect(getInitials('John'), 'J'); // single word
+      expect(getInitials('John Doe'), 'JD'); // 2 words
+      expect(getInitials('John Michael Doe'), 'JM'); // more than 2
+      expect(getInitials('  John   Doe  '), 'JD'); // spaces
+      expect(getInitials('john doe'), 'JD');
+      expect(getInitials('John #Doe'), 'J#'); // non alphabetic
+      expect(getInitials('J'), 'J'); // single char
+      expect(getInitials('A B C D'), 'AB'); // initials with more than 2 chars
+    });
+  });
+}
