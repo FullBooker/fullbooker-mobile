ac53b506ede55d6b444cece15f11ed11602e5526
Abiud Orina <abiudrn@gmail.com>
2025-03-18 22:32:47 +0300

    feat: fix app loading in login page

--- lib/shared/widgets/app_loading.dart
+++ lib/shared/widgets/app_loading.dart
@@ -2,16 +2,15 @@
 import 'package:flutter/material.dart';
 
 class AppLoading extends StatelessWidget {
-  const AppLoading({super.key, this.customColor = Colors.white});
-
-  final Color customColor;
+  const AppLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: CircularProgressIndicator.adaptive(
         strokeWidth: 1,
-        valueColor: AlwaysStoppedAnimation<Color>(customColor),
+        valueColor:
+            AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
       ),
     );
   }
