57b6d52d13483d37d9e5b74e8f6b91eb3e4ce9ec
Abiud Orina <abiudrn@gmail.com>
2025-04-18 16:48:30 +0300

    feat: gracefully handle empty properties in a product details page

--- lib/presentation/host/products/widgets/product_schedule_widget.dart
+++ lib/presentation/host/products/widgets/product_schedule_widget.dart
@@ -4,9 +4,11 @@ import 'package:fullbooker/application/core/services/app_wrapper_base.dart';
 import 'package:fullbooker/application/redux/actions/fetch_product_schedules_action.dart';
 import 'package:fullbooker/application/redux/states/app_state.dart';
 import 'package:fullbooker/application/redux/view_models/product_detail_view_model.dart';
+import 'package:fullbooker/core/common/constants.dart';
 import 'package:fullbooker/core/theme/app_colors.dart';
 import 'package:fullbooker/core/utils.dart';
 import 'package:fullbooker/domain/core/entities/product.dart';
+import 'package:fullbooker/domain/core/entities/product_schedule.dart';
 import 'package:fullbooker/domain/core/value_objects/app_strings.dart';
 import 'package:fullbooker/shared/widgets/app_loading.dart';
 import 'package:heroicons/heroicons.dart';
@@ -28,10 +30,26 @@ class ProductScheduleWidget extends StatelessWidget {
       },
       builder: (BuildContext context, ProductDetailViewModel vm) {
         final Product? product = vm.selectedProduct;
+        final ProductSchedule? schedule = product?.schedule;
+
+        final bool isDateValid = schedule?.startDate != null &&
+            schedule!.startDate!.isNotEmpty &&
+            schedule.startDate != UNKNOWN &&
+            schedule.endDate != null &&
+            schedule.endDate!.isNotEmpty &&
+            schedule.endDate != UNKNOWN;
+
+        final bool isTimeValid = schedule?.startTime != null &&
+            schedule!.startTime!.isNotEmpty &&
+            schedule.startTime != UNKNOWN &&
+            schedule.endTime != null &&
+            schedule.endTime!.isNotEmpty &&
+            schedule.endTime != UNKNOWN;
 
         if (context.isWaiting(FetchProductSchedulesAction)) {
           return AppLoading();
         }
+
         return Wrap(
           spacing: 12,
           runSpacing: 12,
@@ -42,23 +60,29 @@ class ProductScheduleWidget extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               spacing: 4,
               children: <Widget>[
-                HeroIcon(
+                const HeroIcon(
                   HeroIcons.calendar,
                   color: AppColors.greyTextColor,
                   size: 20,
                 ),
-                humanizeDate(
-                  loadedDate: product?.schedule?.startDate ?? '',
-                  dateTextStyle: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  to,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                humanizeDate(
-                  loadedDate: product?.schedule?.endDate ?? '',
-                  dateTextStyle: Theme.of(context).textTheme.bodyMedium,
-                ),
+                if (isDateValid) ...<Widget>[
+                  humanizeDate(
+                    loadedDate: schedule.startDate!,
+                    dateTextStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  Text(
+                    to,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  humanizeDate(
+                    loadedDate: schedule.endDate!,
+                    dateTextStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ] else
+                  Text(
+                    notProvided,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
               ],
             ),
 
@@ -67,23 +91,29 @@ class ProductScheduleWidget extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               spacing: 4,
               children: <Widget>[
-                HeroIcon(
+                const HeroIcon(
                   HeroIcons.clock,
                   color: AppColors.greyTextColor,
                   size: 20,
                 ),
-                formatTime(
-                  time: product?.schedule?.startTime,
-                  textStyle: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  to,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                formatTime(
-                  time: product?.schedule?.endTime,
-                  textStyle: Theme.of(context).textTheme.bodyMedium,
-                ),
+                if (isTimeValid) ...<Widget>[
+                  formatTime(
+                    time: schedule.startTime!,
+                    textStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  Text(
+                    to,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  formatTime(
+                    time: schedule.endTime!,
+                    textStyle: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ] else
+                  Text(
+                    notProvided,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
               ],
             ),
           ],
