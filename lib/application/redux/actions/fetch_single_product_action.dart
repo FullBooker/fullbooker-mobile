8d0e0deee266e1957f9b81b27248e508aef6fd15
Abiud Orina <abiudrn@gmail.com>
2025-04-23 21:01:32 +0300

    feat: add token refresh to commonly visited page
    - will increase chances of token refresh while we find a permanent solution

--- lib/application/redux/actions/fetch_single_product_action.dart
+++ lib/application/redux/actions/fetch_single_product_action.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:async_redux/async_redux.dart';
 import 'package:fullbooker/application/core/services/i_custom_client.dart';
+import 'package:fullbooker/application/redux/actions/check_and_refresh_token_action.dart';
 import 'package:fullbooker/application/redux/actions/update_host_state_action.dart';
 import 'package:fullbooker/application/redux/states/app_state.dart';
 import 'package:fullbooker/core/common/constants.dart';
@@ -49,7 +50,13 @@ class FetchSingleProductAction extends ReduxAction<AppState> {
     final Map<String, dynamic> body =
         json.decode(httpResponse.body) as Map<String, dynamic>;
 
-    if (httpResponse.statusCode >= 400) {
+    final int code = httpResponse.statusCode;
+
+    if (code >= 400) {
+      if (code == 401) {
+        dispatch(CheckAndRefreshTokenAction(client: client));
+      }
+
       final String? error = client.parseError(body);
 
       onError?.call(error ?? defaultUserFriendlyMessage);
