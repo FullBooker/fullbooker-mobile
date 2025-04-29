ffb0f15fdf3cabb4c20bef0daf307131215af20a
Abiud Orina <abiudrn@gmail.com>
2025-04-29 22:04:44 +0300

    feat: fix video upload when editing

--- lib/application/redux/actions/upload_product_videos_action.dart
+++ lib/application/redux/actions/upload_product_videos_action.dart
@@ -5,11 +5,13 @@ import 'package:async_redux/async_redux.dart';
 import 'package:fullbooker/application/core/services/i_custom_client.dart';
 import 'package:fullbooker/application/redux/actions/update_product_action.dart';
 import 'package:fullbooker/application/redux/states/app_state.dart';
+import 'package:fullbooker/application/redux/states/host_state.dart';
 import 'package:fullbooker/core/common/constants.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fullbooker/domain/core/entities/product_media.dart';
 import 'package:fullbooker/domain/core/value_objects/app_config.dart';
 import 'package:fullbooker/domain/core/value_objects/app_strings.dart';
+import 'package:fullbooker/shared/entities/enums.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 
@@ -28,13 +30,23 @@ class UploadProductVideosAction extends ReduxAction<AppState> {
 
   @override
   Future<AppState?> reduce() async {
-    final List<ProductMedia?> existingVideos =
-        state.hostState?.currentProduct?.videos ?? <ProductMedia?>[];
+    final HostState? hostState = state.hostState;
+    final WorkflowState workflowState =
+        hostState?.workflowState ?? WorkflowState.CREATE;
 
-    final String productID = state.hostState?.currentProduct?.id ?? UNKNOWN;
+    final bool isEdit = workflowState == WorkflowState.VIEW;
+
+    final List<ProductMedia?> existingVideos = List<ProductMedia?>.from(
+      isEdit
+          ? hostState?.selectedProduct?.videos ?? <ProductMedia>[]
+          : hostState?.currentProduct?.videos ?? <ProductMedia>[],
+    );
+
+    final String? productID =
+        isEdit ? hostState?.selectedProduct?.id : hostState?.currentProduct?.id;
 
     final Map<String, String> data = <String, String>{
-      'product_id': productID,
+      'product_id': productID ?? UNKNOWN,
       'media_type': kVideoMediaType,
     };
 
