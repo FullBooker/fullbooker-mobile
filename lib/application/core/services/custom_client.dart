08905b9dd78e51f0c50487b9f7cca798bb6017ec
Abiud Orina <abiudrn@gmail.com>
2025-05-04 18:36:36 +0300

    feat: remove token refresh timer
    - this will improve token refresh at the client level

--- lib/application/core/services/custom_client.dart
+++ lib/application/core/services/custom_client.dart
@@ -1,80 +1,91 @@
+import 'package:fullbooker/application/core/services/i_custom_client.dart';
+
+import 'dart:async';
 import 'dart:convert';
 
-import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:fullbooker/application/core/services/i_custom_client.dart';
+import 'package:async_redux/async_redux.dart';
+import 'package:fullbooker/core/utils/utils.dart';
+import 'package:http/http.dart' as http;
 import 'package:fullbooker/application/redux/actions/update_auth_state_action.dart';
 import 'package:fullbooker/application/redux/states/app_state.dart';
 import 'package:fullbooker/application/redux/states/auth_credentials.dart';
-import 'package:fullbooker/core/common/constants.dart';
-import 'package:fullbooker/core/utils/utils.dart';
-
-import 'package:http/http.dart' as http;
 
 class CustomClient extends ICustomClient {
   CustomClient({
     required this.context,
     required String accessToken,
-    this.headers,
-    this.authenticated = true,
-    required this.refreshToken,
     required this.refreshTokenEndpoint,
-  }) {
+    required this.refreshToken,
+    this.headers,
+    http.Client? client,
+  }) : _client = client ?? http.Client() {
     super.accessToken = accessToken;
   }
 
-  final bool authenticated;
   final BuildContext context;
   final Map<String, String>? headers;
   final String refreshToken;
   final String refreshTokenEndpoint;
 
+  final http.Client _client;
+  bool _isRefreshing = false;
+
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    if (authenticated) {
-      final AppState store = StoreProvider.state<AppState>(context);
-      final AuthCredentials? credentials = store.authState?.authCredentials;
-      final String? expiry = credentials?.expiresAt;
+    final bool isSignedIn =
+        StoreProvider.state<AppState>(context).authState?.isSignedIn ?? false;
 
-      final bool isSignedIn = store.authState?.isSignedIn ?? false;
-
-      if (!isSignedIn) return request.send();
-
-      final DateTime now = DateTime.now();
-
-      final DateTime expiresAt =
-          DateTime.tryParse(expiry ?? '')?.toLocal() ?? now;
-
-      final bool hasExpired = hasTokenExpired(expiresAt, now);
-
-      final bool shouldRefreshToken =
-          (credentials?.accessToken?.isEmpty ?? true) ||
-              credentials?.accessToken == UNKNOWN ||
-              hasExpired;
-
-      if (shouldRefreshToken) {
-        final AuthCredentials? refreshedCredentials =
-            await _refreshUserToken(credentials!.refreshToken!);
-
-        if (refreshedCredentials != null) {
-          StoreProvider.dispatch<AppState>(
-            context,
-            UpdateAuthStateAction(
-              accessToken: refreshedCredentials.accessToken,
-              refreshToken: refreshedCredentials.refreshToken,
-              expiresAt: refreshedCredentials.expiresAt,
-            ),
-          );
-
-          super.accessToken = refreshedCredentials.accessToken!;
-        }
-      }
+    if (!isSignedIn) {
+      request.headers.addAll(<String, String>{...?headers});
+      return _client.send(request);
     }
 
-    return request.send();
+    request.headers.addAll(<String, String>{...?headers});
+    final String? expiryDateString = StoreProvider.state<AppState>(context)
+        .authState
+        ?.authCredentials
+        ?.expiresAt;
+
+    final DateTime expiryDateTime =
+        DateTime.tryParse(expiryDateString ?? '') ?? DateTime.now();
+
+    final bool shouldRefreshToken =
+        hasTokenExpired(expiryDateTime, DateTime.now()) || refreshToken.isEmpty;
+
+    if (!shouldRefreshToken) {
+      return _client.send(request);
+    }
+
+    // Prevent multiple refresh attempts for the same request
+    if (_isRefreshing) {
+      return _client.send(request);
+    }
+
+    _isRefreshing = true;
+
+    final AuthCredentials? newAuthCredentials = await _refreshUserToken();
+
+    if (newAuthCredentials != null) {
+      request.headers.addAll(<String, String>{
+        'Authorization': 'Bearer ${newAuthCredentials.accessToken}',
+      });
+
+      context.dispatch(
+        UpdateAuthStateAction(
+          accessToken: newAuthCredentials.accessToken,
+          refreshToken: newAuthCredentials.refreshToken,
+          expiresAt: newAuthCredentials.expiresAt,
+        ),
+      );
+    }
+
+    _isRefreshing = false;
+
+    return _client.send(request);
   }
 
-  Future<AuthCredentials?> _refreshUserToken(String refreshToken) async {
+  Future<AuthCredentials?> _refreshUserToken() async {
     final String endpoint = refreshTokenEndpoint;
     final Map<String, dynamic> body = <String, dynamic>{
       'refresh': refreshToken,
