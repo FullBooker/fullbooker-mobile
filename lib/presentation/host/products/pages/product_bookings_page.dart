427a3971c8bc4c3397ee4b521153993272aba3a2
Abiud Orina <abiudrn@gmail.com>
2025-05-16 15:34:20 +0300

    feat: fix product bookings search

--- lib/presentation/host/products/pages/product_bookings_page.dart
+++ lib/presentation/host/products/pages/product_bookings_page.dart
@@ -16,6 +16,7 @@ import 'package:fullbooker/presentation/core/components/custom_app_bar.dart';
 import 'package:fullbooker/presentation/core/components/generic_zero_state.dart';
 import 'package:fullbooker/presentation/host/products/widgets/booking_list_item_widget.dart';
 import 'package:fullbooker/presentation/host/products/widgets/search_product_bookings_input.dart';
+import 'package:fullbooker/presentation/shared/custom_bottom_nav_container.dart';
 import 'package:fullbooker/shared/widgets/app_loading.dart';
 import 'package:fullbooker/shared/widgets/primary_button.dart';
 import 'package:heroicons/heroicons.dart';
@@ -36,8 +37,7 @@ class ProductBookingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: productBookings),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+      bottomNavigationBar: CustomBottomNavContainer(
         child: PrimaryButton(
           onPressed: () => context.router.push(ScanTicketsRoute()),
           customRadius: 100,
@@ -107,6 +107,7 @@ class ProductBookingsPage extends StatelessWidget {
                 ),
 
                 // TODO(abiud): add filter groups here
+                SearchProductBookingsInput(),
 
                 StoreConnector<AppState, ProductsBookingsViewModel>(
                   converter: (Store<AppState> store) =>
@@ -142,28 +143,21 @@ class ProductBookingsPage extends StatelessWidget {
                       );
                     }
 
-                    return Column(
-                      spacing: 16,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SearchProductBookingsInput(),
-                        ListView.builder(
-                          itemCount: vm.bookings?.length,
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          padding: EdgeInsets.only(bottom: 100),
-                          itemBuilder: (BuildContext context, int index) {
-                            final Booking? current = vm.bookings![index];
+                    return ListView.builder(
+                      itemCount: vm.bookings?.length,
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      padding: EdgeInsets.only(bottom: 100),
+                      itemBuilder: (BuildContext context, int index) {
+                        final Booking? current = vm.bookings![index];
 
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 4,
-                              ),
-                              child: BookingListItem(booking: current!),
-                            );
-                          },
-                        ),
-                      ],
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 4,
+                          ),
+                          child: BookingListItem(booking: current!),
+                        );
+                      },
                     );
                   },
                 ),
